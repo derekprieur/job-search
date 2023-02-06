7ae6908c47f50a9b552892aba4ab572e9b6b6ffb
derekprieur <DPrieur123@gmail.com>
2023-02-06 10:27:48 -0500

    chart js updates

--- pages/salaries/index.js
+++ pages/salaries/index.js
@@ -1,13 +1,13 @@
-import { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import Image from "next/image";
 
 import { Navbar, SalaryInput, Title } from "components";
-import { update } from '../../redux/isMobileSlice'
 import chartlight from '../../assets/chartlight.png'
 import chartdark from '../../assets/chartdark.png'
 import chartlightlarge from '../../assets/chartlightlarge.png'
 import chartdarklarge from '../../assets/chartdarklarge.png'
+import { useEffect, useState } from "react";
+import BarChart from "../../utils/barchart";
 
 export default function Salaries() {
     const isDark = useSelector(state => state.darkMode.value)
@@ -15,7 +15,7 @@ export default function Salaries() {
     return (
         <main>
             <Navbar />
-            <div className={`${!isDark ? 'bg-[#FAFAFB]' : 'bg-[#13131A]'}  h-full py-20 md:flex md:gap-20 md:px-20 px-6`}>
+            <div className={`${!isDark ? 'bg-[#FAFAFB]' : 'bg-[#13131A]'}  h-full md:h-screen py-20 md:flex md:gap-20 md:px-20 px-6`}>
                 <div className="md:flex-col md:w-[50%]">
                     <div className="mt-6">
                         <Title title="Estimated Salaries" />
@@ -28,17 +28,8 @@ export default function Salaries() {
                         </div>
                     </div>
                 </div>
-                <div className="md:w-[50%] md:flex md:items-end md:justify-end">
-                    <div className="block md:hidden">
-                        {isDark
-                            ? <Image src={chartdark} alt='chart' className="w-full object-contain mt-10 md:hidden" />
-                            : <Image src={chartlight} alt='chart' className="w-full object-contain mt-10 md:hidden" />}
-                    </div>
-                    <div className="hidden md:block">
-                        {isDark
-                            ? <Image src={chartdarklarge} alt='chart' className="w-full object-contain mt-10 " />
-                            : <Image src={chartlightlarge} alt='chart' className="w-full object-contain mt-10 " />}
-                    </div>
+                <div className="md:w-[50%] md:flex md:pt-12">
+                    <BarChart />
                 </div>
             </div>
 
---
+++ utils/barchart.js
@@ -0,0 +1,61 @@
+import { Bar } from 'react-chartjs-2'
+import { Chart as ChartJS, CategoryScale, LinearScale, Title as ChartTitle, Legend, Tooltip, BarElement } from 'chart.js'
+import { useEffect, useState } from 'react'
+
+ChartJS.register(CategoryScale, LinearScale, ChartTitle, Legend, Tooltip, BarElement)
+
+const BarChart = () => {
+    const [chartData, setChartData] = useState({
+        datasets: [],
+    })
+    const [chartOptions, setChartOptions] = useState({})
+
+    useEffect(() => {
+        setChartData({
+            labels: ['Salary.com', 'ZipRecruiter', 'Talent.com'],
+            datasets: [
+                {
+                    label: 'Minimum Salary',
+                    data: [97000, 104000, 107000,],
+                    borderColor: 'rgb(53, 162, 235)',
+                    backgroundColor: '#FDDD8C',
+                },
+                {
+                    label: 'Maximum Salary',
+                    data: [121000, 123000, 122000,],
+                    borderColor: 'rgb(50,50,50)',
+                    backgroundColor: 'rgba(11, 171, 124, 0.7',
+                },
+                {
+                    label: 'Median Salary',
+                    data: [112000, 117000, 115000,],
+                    borderColor: 'rgb(50,50,50)',
+                    backgroundColor: '#FFBBD7',
+                },
+            ],
+        })
+        setChartOptions({
+            plugins: {
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Estimated Salary for NodeJS Developer in New York',
+                },
+            },
+            maintainAspectRatio: false,
+            responsive: true,
+        })
+    }, [])
+
+    return (
+        <>
+            <div className='w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white'>
+                <Bar data={chartData} options={chartOptions} />
+            </div>
+        </>
+    )
+}
+
+export default BarChart
