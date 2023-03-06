14c1b3b98b1ec17d559504a22115573a25efe32e
Derek Prieur <DPrieur123@gmail.com>
2023-03-06 16:35:43 -0500

    updates

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,6 +7,7 @@ export const authOptions = {
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorizationUrl: "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
         }),
         // ...add more providers here
     ],
